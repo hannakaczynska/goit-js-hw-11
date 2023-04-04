855e28c21d8e73cf53bd3f9e538c0ed402243fdc
hannakaczynska <hannakacz13@gmail.com>
2023-04-04 18:22:08 +0200

    added

---
+++ src/gallery.js
@@ -0,0 +1,22 @@
+// const createPhotosList = photos => {
+//   const photosList = photos.map(
+//     photo => `<div class="photo-card">
+//   <img src="${photo.webformatURL}" alt="${photo.keys}" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b> ${photo.likes}
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b> ${photo.views}
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b> ${photo.comments}
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b> ${photo.downloads}
+//     </p>
+//   </div>
+// </div>`
+//   );
+//   innerHTML = photosList
+// };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import axios, { isCancel, AxiosError } from 'axios';
+import axios from 'axios';
 import Notiflix from 'notiflix';
 
 const BASE_URL = 'https://pixabay.com/api/';
@@ -11,46 +11,59 @@ const params = new URLSearchParams({
 });
 
 let searchedPhoto = 'cat';
-
 const url = `${BASE_URL}?key=${MY_KEY}&q=${searchedPhoto}&${params}`;
 
-//PROBLEM WITH AXIOS!!!!!!!!!!!!
+const gallery = document.querySelector('.gallery');
 
-// const fetchPhotos = async () => {
-//   try {
-//     const response = await axios.get(
-//       `${BASE_URL}?key=${MY_KEY}&q=${searchedPhoto}&${params}`
-//     );
-//     const photos = await response.json();
-//     return photos;
-//     console.log(photos);
-//   } catch (error) {
-//     Notiflix.Notify.failure(
-//       'Sorry, there are no images matching your search query. Please try again.'
-//     );
-//   }
-// };
+const createPhotosList = photos => {
+  const photosList = photos
+    .map(
+      photo => `<div class="photo-card">
+  <img src="${photo.webformatURL}" alt="${photo.keys}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> ${photo.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${photo.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b> ${photo.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> ${photo.downloads}
+    </p>
+  </div>
+</div>`
+    )
+    .join('');
+  gallery.innerHTML = photosList;
+};
 
-// fetchPhotos();
+const managePhotosData = photos => {
+  if (photos.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else {
+    createPhotosList(photos);
+    console.log(photos);
+  }
+};
 
-fetch(`${BASE_URL}?key=${MY_KEY}&q=${searchedPhoto}&${params}`)
-  .then(response => {
-    if (!response.ok) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-    return response.json();
-  })
-  .then(data => {
-    if (data.hits.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-    console.log(data.hits);
-  });
+const getPhotosData = async () => {
+  try {
+    const response = await axios.get(url);
+    const photos = await response.data.hits;
+    managePhotosData(photos);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+getPhotosData();
+
+//FETCH WITHOUT ASYNC/AWAIT
 // fetch(url)
 //   .then(response => {
 //     if (!response.ok) {
@@ -69,11 +82,19 @@ fetch(`${BASE_URL}?key=${MY_KEY}&q=${searchedPhoto}&${params}`)
 //     console.log(data.hits);
 //   });
 
-// fetch(url)
-//   .then(response => {
-//     if (!response.ok) {
-//       Notiflix.Notify.failure('OOPS, there is no country with that name');
-//     }
-//     return response.json();
-//   })
-//   .then(data => console.log(data));
+// const fetchPhotos = async () => {
+//   try {
+//     const response = await axios.get(
+//       `${BASE_URL}?key=${MY_KEY}&q=${searchedPhoto}&${params}`
+//     );
+//     const photos = await response.json();
+//     // return photos;
+//     console.log(photos);
+//   } catch (error) {
+//     Notiflix.Notify.failure(
+//       'Sorry, there are no images matching your search query. Please try again.'
+//     );
+//   }
+// };
+
+// fetchPhotos();
