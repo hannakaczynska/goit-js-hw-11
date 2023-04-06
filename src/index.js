354bd4c9f7e811d62c11b72102a2d3b911d92a1a
hannakaczynska <hannakacz13@gmail.com>
2023-04-06 17:08:12 +0200

    added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
